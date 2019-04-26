79f4472202b88bb6fa453b797ab995d12120bb31
ikens <medivh920920@gmail.com>
2019-04-26 22:22:03 +0800

    spring官方文档Restful接口demo

--- src/main/java/org/iken/main/RestfulMain.java
+++ src/main/java/org/iken/main/RestfulMain.java
@@ -1,9 +1,13 @@
 package org.iken.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RestfulMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(RestfulMain.class, args);
 	}
 
 }
