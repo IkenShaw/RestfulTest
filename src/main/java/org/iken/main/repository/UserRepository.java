f4c98acdd9c4d941b0056d1ce492c7ba4894fc9f
IkenS <medivh920920@gmail.com>
2019-05-03 12:32:07 +0800

    移除Spring jpa所有文件到新仓库

--- src/main/java/org/iken/main/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.iken.main.repository;
-
-import org.iken.main.vo.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-
-}
